099c14c1bb583bd9d26d567e3af9b258fdba203e
Tom <tombrandisicloud@gmail.com>
2023-12-19 20:50:29 +0000

    add writer logins

---
+++ writer/login.php
@@ -0,0 +1,56 @@
+<?php
+/*
+CREATE TABLE "writers" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"username"	TEXT,
+	"passwordhash"	TEXT,
+	"permissionLevel"	INTEGER,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+*/
+if(isset($_POST["username"]))
+{
+    $stmt = $pdo->prepare("SELECT [passwordhash] FROM writers WHERE [username]=?");
+    $stmt->execute(array($_POST["username"]));
+    $passwordhash = $stmt->fetch()[0];
+
+    if(isset($passwordhash) && password_verify($_POST["password"], $passwordhash))
+    {
+        $stmt = $pdo->prepare("SELECT [username], [permissionLevel] FROM writers WHERE [username]=?");
+        $stmt->execute(array($_POST["username"]));
+        $_SESSION = $stmt->fetch();
+        echo '<script type="text/javascript">
+        window.location = "";
+        </script>';
+    }
+}
+else if(isset($_POST["newusername"]))
+{
+    $passwordhash = password_hash($_POST["newpassword"], PASSWORD_DEFAULT);
+    $stmt = $pdo->prepare("INSERT INTO writers ([username], [passwordhash], [permissionLevel]) VALUES (?, ?, 1)");
+    $stmt->execute(array($_POST["newusername"], $passwordhash));
+    $passwordhash = $stmt->fetch();
+}
+?>
+
+<h2>Login</h2>
+<form  action="" method="POST">
+    <label >Username
+        <input  name="username">
+    </label>
+    <label >Password
+        <input name="password" type="password">
+    </lable>
+    <input class="login button" type="submit" value="login">
+</form>
+
+<h2>New writer</h2>
+<form  action="" method="POST">
+    <label >Username
+        <input  name="newusername">
+    </label>
+    <label >Password
+        <input name="newpassword" type="password">
+    </lable>
+    <input class="login button" type="submit" value="create">
+</form>
--- writer/write.php
+++ writer/write.php
@@ -1,29 +1,35 @@
-<?php if($pageinfo):?>
-<link rel="stylesheet" type="text/css" href="/write-frontend/write.css" />
-<div id="page">
-    <div class="headerblock">
-        <div class="writeheader">
-        <?php
-        echo 'write.php';
-        echo $article;
-        $blockarray = json_decode($pageinfo["blocks"]);
-        ?>
-        </div>
-    </div>
 <?php
-echo Blocks2Html($blockarray, true);
-?>
-</div>
-<div id="styleModifiers">
-    <input type="button" value="n" class="styleModifier" onclick="addTextStyle('normal')">
-    <input type="button" value="B" class="styleModifier" onclick="addTextStyle('bold')">
-    <input type="button" value="I" class="styleModifier" onclick="addTextStyle('italic')">
-</div>
-<div id="writeExtras">
-    <input type="button" value="save" onclick="Save()">
-    <input type="button" value="remove" onclick="deleteSelected()">
-    <script defer src="/write-frontend/write.js"></script>
-</div>
-<?php else:?>
-    <h1>write overview</h1>
+session_start();
+if(isset($_SESSION["permissionLevel"]) && $_SESSION["permissionLevel"] >= 1):  //IF logged in?>
+    <?php if($pageinfo):?>
+    <link rel="stylesheet" type="text/css" href="/write-frontend/write.css" />
+    <div id="page">
+        <div class="headerblock">
+            <div class="writeheader">
+            <?php
+            echo 'write.php';
+            echo $article;
+            $blockarray = json_decode($pageinfo["blocks"]);
+            ?>
+            </div>
+        </div>
+    <?php
+    echo Blocks2Html($blockarray, true);
+    ?>
+    </div>
+    <div id="styleModifiers">
+        <input type="button" value="n" class="styleModifier" onclick="addTextStyle('normal')">
+        <input type="button" value="B" class="styleModifier" onclick="addTextStyle('bold')">
+        <input type="button" value="I" class="styleModifier" onclick="addTextStyle('italic')">
+    </div>
+    <div id="writeExtras">
+        <input type="button" value="save" onclick="Save()">
+        <input type="button" value="remove" onclick="deleteSelected()">
+        <script defer src="/write-frontend/write.js"></script>
+    </div>
+    <?php else:?>
+        <h1>write overview</h1>
+    <?php endif?>
+<?php else: // log in?>
+    <?php include 'writer/login.php'?>
 <?php endif?>
