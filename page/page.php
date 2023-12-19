164633b12b2a4f2dbb6ce6cad807905349b23eb2
Tom <tombrandisicloud@gmail.com>
2023-12-19 18:11:58 +0000

    add 404 when page doesnt exist

--- page/page.php
+++ page/page.php
@@ -1,8 +1,15 @@
 <?php
-$blockarray = json_decode($pageinfo["blocks"]);
-
-echo '<div id="page">';
-echo Blocks2Html($blockarray);
-echo '</div>';
+if($pageinfo)
+{
+    $blockarray = json_decode($pageinfo["blocks"]);
+    
+    echo '<div id="page">';
+    echo Blocks2Html($blockarray);
+    echo '</div>';
+}
+else
+{
+    echo '404 - page not found';
+}
 
 ?>
