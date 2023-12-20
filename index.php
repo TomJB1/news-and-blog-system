b9396219feaec3a1a9dce663d99049b14898aa2f
Tom <tombrandisicloud@gmail.com>
2023-12-20 12:32:41 +0000

    add copyright (copyleft) notice to index

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<!---
+This file is part of news-and-blog-system, a program for creating blog or news sites.
+Copyright (C) 2023 Tom Brandis
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License Version 3 as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-->
 <!DOCTYPE html>
 <html lang="en">
 <head>
