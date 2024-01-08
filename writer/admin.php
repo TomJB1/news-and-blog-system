cc86c69234667242e5bec6306dc37e846b52a3df
Tom <tombrandisicloud@gmail.com>
2024-01-08 17:57:06 +0000

    add a add writer section to admin page

--- writer/admin.php
+++ writer/admin.php
@@ -8,8 +8,20 @@
         echo '<script type="text/javascript">
         window.location = "";
         </script>';
-    }    
+    }
     ?>
+
+<h2>Writers</h2>
+<?php
+$stmt = $pdo->prepare("SELECT [username] FROM writers WHERE [permissionLevel]=1");
+$stmt->execute();
+$writers = $stmt->fetchAll();
+foreach($writers as $id => $writer)
+{
+    echo $writer["username"];
+    echo "<br>";
+}
+?>
 
 <h2>New writer</h2>
 <form  action="" method="POST">
